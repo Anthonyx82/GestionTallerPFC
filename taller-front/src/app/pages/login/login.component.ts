bd9f6f3477c60e54e827d7b877cc4ca718ebf54f
Anthonyx82 <antoniomartinmanzanares2004@gmail.com>
2025-03-17 18:17:39 +0100

    Login corregido

--- taller-front/src/app/pages/login/login.component.ts
+++ taller-front/src/app/pages/login/login.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common'; // <-- IMPORTAR CommonModule
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule], // <-- IMPORTAR FormsModule AQUÍ
+  imports: [FormsModule, CommonModule], // <-- AÑADIR CommonModule AQUÍ
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
