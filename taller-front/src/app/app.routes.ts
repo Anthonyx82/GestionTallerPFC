59ff881c79e8ade6c54f166742c5812731b69a37
anthonyx82-admin <amartinsos@minsait.com>
2025-03-19 09:35:42 +0100

    Correccion fallo landing

--- taller-front/src/app/app.routes.ts
+++ taller-front/src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './pages/register/register.component';
 import { MisVehiculosComponent } from './mis-vehiculos/mis-vehiculos.component';
 import { LoginComponent } from './pages/login/login.component';
-import { AppComponent } from './app.component';
 import { EditarVehiculoComponent } from './editar-vehiculo/editar-vehiculo.component';
 
 export const routes: Routes = [
@@ -10,5 +9,4 @@ export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'mis-vehiculos', component: MisVehiculosComponent },
     { path: 'editar-vehiculo/:id', component: EditarVehiculoComponent },
-    { path: '', component: AppComponent },
 ];
